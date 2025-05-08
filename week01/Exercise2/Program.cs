7f927b0e22a8e3a543c49220a8f72d269cbc30f2
monvicalburo <monvicalburo@gmal.com>
2025-05-08 21:04:26 +0800

    Done with exercise 2 Core Requirements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,47 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine();
+
+        Console.Write("Enter your grade percentage: ");
+        string gradePercentage = Console.ReadLine();
+        Console.WriteLine();
+
+        int grade = int.Parse(gradePercentage);
+        string letter = "";
+
+        if ( grade >= 90)
+        {
+            letter = "A";
+        }
+        else if ( grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course. ");
+        }
+        else
+        {
+            Console.WriteLine ("You failed the course. Study harder to pass next time! ");
+        }
+
+
     }
 }
